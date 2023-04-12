366981e2adbfc0489c174a8301c9c0aa498e02fb
Numeroh <astras.ctto@gmail.com>
2023-04-12 00:00:33 -0300

    Server express y Query
    Se crea la query y busqueda de productos creados en archivo Productos.txt

--- src/app.js
+++ src/app.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
+const fs = require('fs');
+const { reset } = require('nodemon');
 
-app.get('/saludo', (req, res) => {
-    res.send("Hola mundungus. Te manda saludo ojoloco moody");
+const products = JSON.parse(fs.readFileSync(__dirname + "/Productos.txt"));
+
+
+app.get('/productos', (req, res) => {
+    let limite = req.query.limit;
+    if (!limite || products.length == limite) {
+        res.send(products);
+    }
+    else if (products.length > limite) {
+        res.send(products.slice(0, limite));
+    }
+    else if (products.length < limite) {
+        res.status(404).send("Limite excedido.");
+    }
 });
 
-app.listen(8080, () => {
-    console.log("Servidor escuchando en el puerto 8080");
-});
